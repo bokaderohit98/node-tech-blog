d8718426b2936fe4811da8f4e5ddda662913040b
Rohit Bokade <bokaderohit98@gmail.com>
2018-03-09 22:27:34 +0530

    added partials and main layout

--- server/app.js
+++ server/app.js
@@ -1,36 +1,39 @@
 const exhbs = require('express-handlebars');
 const path = require('path');
 const express = require('express');
-const body_parser = require('body-parser');
+const bodyParser = require('body-parser');
 
 //adding dependencies
 require('./config/config');
 const nodemailer = require('./config/nodemailer');
 const mongoose = require('./config/mongoose');
 
-//setting port
-const port = process.env.PORT;
-
 //initializing app
 const app = express();
+
+//Adding static folder
+app.use(express.static(path.join(__dirname, '../public')));
 
 //setting view engine
 app.engine('handlebars', exhbs({
   defaultLayout: 'main'
 }));
-app.set(view-engine, 'handlebars');
+app.set('view engine', 'handlebars');
 
-//setting up middle wares
+//setting up middlewares
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(bodyParser.json());
 
-
 //Home route
 app.get('/', (req, res) => {
-  res.send('App started');
+  console.log('home');
+  res.render('index');
 });
+
+//setting port
+const port = process.env.PORT;
 
 //starting app
 app.listen(port, (err) => {
